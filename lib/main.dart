7e15f332a5e710c60e6544095b1c6f9291677bd6
miledoudaimi <78927431+miledoudaimi@users.noreply.github.com>
2021-02-25 21:37:41 -0500

    initiailsation projet
    allo

---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+void main() =>runApp(MaterialApp(
+  home: Scaffold(
+    appBar: AppBar(
+      title: Text('Techno Quiz'),
+      centerTitle: true,
+      backgroundColor: Colors.deepOrange,
+    ),
+    body: Center(
+
+       child: Text(
+           'Test',
+         style: TextStyle(
+           fontSize: 20.0,
+           fontWeight: FontWeight.bold,
+           letterSpacing: 2.0,
+           color: Colors.black,
+         ),
+       )
+        ),
+    floatingActionButton: FloatingActionButton(
+      onPressed: (){},
+      child: Text('cliquer'),
+      backgroundColor: Colors.green,
+    ),
+  ),
+
+));
+
+
