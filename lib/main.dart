de336b69388efb46cb3df8feb3c43ffe00f59467
miledoudaimi <78927431+miledoudaimi@users.noreply.github.com>
2021-03-04 21:06:34 -0500

    test

--- lib/main.dart
+++ lib/main.dart
@@ -1,31 +1,33 @@
 import 'package:flutter/material.dart';
 
 void main() =>runApp(MaterialApp(
-  home: Scaffold(
-    appBar: AppBar(
-      title: Text('Techno Quiz'),
-      centerTitle: true,
-      backgroundColor: Colors.deepOrange,
-    ),
-    body: Center(
-//
-       child: Text(
-           'Test',
-         style: TextStyle(
-           fontSize: 20.0,
-           fontWeight: FontWeight.bold,
-           letterSpacing: 2.0,
-           color: Colors.black,
-         ),
-       )
-        ),
-    floatingActionButton: FloatingActionButton(
-      onPressed: (){},
-      child: Text('cliquer'),
-      backgroundColor: Colors.green,
-    ),
-  ),
+    home: MenuPrincipal(),
 
 ));
+class MenuPrincipal extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+
+   return Scaffold(
+     appBar: AppBar(
+       title: Text('Techno Quiz'),
+       centerTitle: true,
+       backgroundColor: Colors.deepOrange,
+     ),
+     body: Center(
+       ///
+         child: Image(
+         //  image: NetworkImage('https://files.pitchbook.com/website/images/ar/og/b/GettyImages-801481260_vertical.png'),
+         image: AssetImage('assets/123.jpg'),
+         )
+     ),
+     floatingActionButton: FloatingActionButton(
+       onPressed: (){},
+       child: Text('cliquer '),
+       backgroundColor: Colors.green,
+     ),
+   );
+  }
+}
 
 
